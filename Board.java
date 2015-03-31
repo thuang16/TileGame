d8e827aab989c044983587a8e599dc2db25f674e
thuang16 <tingxuanhuang@hotmail.com>
2015-03-31 13:07:13 -1000

    Work Log #7
    I had a ton of trouble getting the graphics to work, so I scrapped the
    graphics portion and is basing the game off a basic array. After I make
    the algorithm and have the game work, I will try to implement the
    graphics portion. Today I spent the class getting making the shuffle
    method.

--- Board.java
+++ Board.java
@@ -1,15 +1,103 @@
+import java.util.Random;
+
 public class Board 
 {
-  public static void board()
+   int[][] theBoard =  {{1,2,3},
+                       {4,5,6},
+                       {7,8,0}};
+   
+  public int[][] getBoard()
   {
-    DividePicture canvas = new DividePicture("640x480.jpg");
-    for(int k=1;k<9;k++)
-    canvas.divideTile(k);
-    canvas.explore();
+    return theBoard;
   }
-  public static void main(String[] args)
-  {
+ 
 
-     board();
+  public String direction(int row, int col)
+  {
+      if (Math.random()<(1/2))
+      {
+        if (Math.random()<(1/2))
+          return "up";
+        else
+          return "down";
+      }
+      else
+      {
+        if (Math.random()<(1/2))
+          return "left";
+        else
+          return "right";
+      }
+  }    
+  public void shuffle()
+  {
+    int temp = 0;
+    for(int row= 0; row<3; row++){
+      for(int col=0; col<3; col++){
+        if(theBoard[row][col] == 0)
+        {
+           temp = theBoard[row][col];
+          if(direction(row, col).equals("up"))
+          {
+            if(row+1>2)
+            {
+              theBoard[row][col] = theBoard[row-1][col];
+              theBoard[row-1][col]= temp;
+            }
+            else
+            {
+              theBoard[row][col] = theBoard[row+1][col];
+              theBoard[row+1][col]= temp;
+            }
+             if(direction(row, col).equals("down"))
+          {
+            if(row-1<0)
+            {
+              theBoard[row][col] = theBoard[row+1][col];
+              theBoard[row+1][col]= temp;
+            }
+            else
+            {
+              theBoard[row][col] = theBoard[row-1][col];
+              theBoard[row-1][col]= temp;
+            }
+             if(direction(row, col).equals("left"))
+          {
+            if(row-1<0)
+            {
+              theBoard[row][col] = theBoard[row][col+1];
+              theBoard[row][col+1]= temp;
+            }
+            else
+            {
+              theBoard[row][col] = theBoard[row][col-1];
+              theBoard[row][col-1]= temp;
+            }
+          }
+             if(direction(row, col).equals("right"))
+          {
+            if(row+1<2)
+            {
+              theBoard[row][col] = theBoard[row][col-1];
+              theBoard[row][col-1]= temp;
+            }
+            else
+            {
+              theBoard[row][col] = theBoard[row][col+1];
+              theBoard[row][col+1]= temp;
+            }
+          }
+          }
+          }
+        }
+      }
+    }
   }
+  
+              
+              
+             
+           
+
 }
+  
---
+++ TileGame.java
@@ -0,0 +1,7 @@
+public class TileGame
+{
+    public static void main(String args [])
+    {
+      Board game = new Board;
+    }
+}
